ffd75d136fe1099867d85f19943f559fbd0f97c0
Richa Khandelwal <richak3@gmail.com>
2018-01-28 21:50:06 -0800

    removed unnecessary loop

--- src/ArrayManipulation.java
+++ src/ArrayManipulation.java
@@ -1,7 +1,15 @@
 import java.util.Scanner;
 
 /**
- * Created by ricks on 1/28/18.
+ You are given a list(1-indexed) of size , initialized with zeroes. You have to perform  operations on the list and output the maximum of final values of all the  elements in the list. For every operation, you are given three integers ,  and  and you have to add value  to all the elements ranging from index  to (both inclusive).
+
+ For example, consider a list  of size . The initial list would be  = [, , ] and after performing the update  = , the new list would be  = [, , ]. Here, we've added value 30 to elements between indices 2 and 3. Note the index of the list starts from 1.
+
+ Input Format
+
+ The first line will contain two integers  and  separated by a single space.
+ Next  lines will contain three integers ,  and  separated by a single space.
+ Numbers in list are numbered from  to .
  */
 public class ArrayManipulation {
 
@@ -10,10 +18,6 @@ public class ArrayManipulation {
         int n = in.nextInt();
 
         long [] list = new long[n];
-
-        for (int i = 0;  i < n; i++ ) {
-            list[i] = 0;
-        }
 
         int m = in.nextInt();
 
