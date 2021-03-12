2a57ddc735d1d5b4f839d98ed78939c46716724c
Richa Khandelwal <richak3@gmail.com>
2021-03-11 17:16:16 -0800

    Optimized solution

--- src/MissingNumbers.java
+++ src/MissingNumbers.java
@@ -1,28 +1,27 @@
-import scala.Int;
-
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class MissingNumbers {
 
     // Complete the missingNumbers function below.
     static int[] missingNumbers(int[] arr, int[] brr) {
-        Map<Integer, Integer> numOfOccurArr = numberOfOcc(arr);
         Map<Integer, Integer> numOfOccurBrr = numberOfOcc(brr);
-
-        Set<Integer> result = new HashSet<>();
-        for (int i=0; i <brr.length; i++) {
-            if(!numOfOccurArr.containsKey(brr[i]) || numOfOccurArr.get(brr[i]) != numOfOccurBrr.get(brr[i])) {
-                result.add(brr[i]);
+        for (int i=0; i <arr.length; i++) {
+             if (numOfOccurBrr.containsKey(arr[i])) {
+                int newNumber =  numOfOccurBrr.get(arr[i]) - 1;
+                if (newNumber <= 0) {
+                    numOfOccurBrr.remove(arr[i]);
+                } else {
+                    numOfOccurBrr.put(arr[i], newNumber);
+                }
             }
         }
 
-        return result.stream().sorted().mapToInt(i -> i).toArray();
+        return numOfOccurBrr.keySet().stream().sorted().mapToInt(i -> i).toArray();
 
     }
 
